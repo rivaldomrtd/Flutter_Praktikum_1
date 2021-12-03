b4c273968a8bdd81617df303e5ae233e723ae480
rivaldomrtd <rivaldomrtd@gmail.com>
2021-12-03 18:08:53 +0700

    3

--- praktikum_3/lib/auth/register.dart
+++ praktikum_3/lib/auth/register.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum_3/auth/loginpage.dart';
+import 'package:praktikum_3/home/home.dart';
+import 'package:praktikum_3/validator/validator.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _registerFormKey = GlobalKey<FormState>();
+  final _nameTextController = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+  final _focusName = FocusNode();
+  final _focusEmail = FocusNode();
+  final _focusPassword = FocusNode();
+  bool _isProcessing = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +54,98 @@ class RegisterPage extends StatelessWidget {
                   shape: BoxShape.circle,
                 ),
               ),
+              Form(
+                key: _registerFormKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _nameTextController,
+                      focusNode: _focusName,
+                      validator: (value) => Validator.validateName(
+                        name: value,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        errorBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _emailTextController,
+                      focusNode: _focusEmail,
+                      validator: (value) => Validator.validateEmail(
+                        email: value,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        errorBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _passwordTextController,
+                      focusNode: _focusPassword,
+                      obscureText: true,
+                      validator: (value) => Validator.validatePassword(
+                        password: value,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        errorBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    _isProcessing
+                        ? CircularProgressIndicator()
+                        : Row(children: [
+                            Expanded(
+                                child: ElevatedButton(
+                              onPressed: () async {
+                                final String name =
+                                    _nameTextController.text.trim();
+                                final String email =
+                                    _emailTextController.text.trim();
+                                final String password =
+                                    _passwordTextController.text.trim();
+                                setState(() {
+                                  _isProcessing = true;
+                                });
+                                if (_nameTextController.text != null &&
+                                    _emailTextController.text != null &&
+                                    _passwordTextController.text != null) {
+                                  Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                      builder: (context) => Home(),
+                                    ),
+                                    ModalRoute.withName('/'),
+                                  );
+                                }
+                              },
+                              child: Text(
+                                'Sign up',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ))
+                          ])
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- praktikum_3/lib/home/home.dart
+++ praktikum_3/lib/home/home.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:praktikum_3/home/home.dart';
 
 class Home extends StatefulWidget {
   @override
